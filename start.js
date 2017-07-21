39566325dd0f209a666ab17b2f6ca61f69fa5965
unknown <nikhileshyadav000@gmail.com>
2017-07-21 19:39:45 +0530

    Port Setting corrected

--- start.js
+++ start.js
@@ -63,10 +63,12 @@ var Users={}
 			res.end(getHTML(data))
 		}
 	})
-var server = app.listen(8000, function () {
+	
+var host = server.address().address
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function () {
 
-   var host = server.address().address
-   var port = server.address().port
+   
 
    console.log("Example app listening at http://%s:%s", host, port)
 })
